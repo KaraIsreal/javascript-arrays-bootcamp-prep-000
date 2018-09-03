d263cf62f7329754259bdc238806e28e1c360a44
Kara Isreal <isreal.kara92@gmail.com>
2018-09-03 21:09:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,36 +5,6 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-   array.unshift("element")
-   return []
-}
-function addElementToEndOfArray (array, element) {
-  return [array, ...element]
-}
-function destructivelyAddElementToEndOfArray (array, element) {
-  array.push("element")
-  return []
-}
-function accessElementInArray(array, index){
-  console.log(array[index])
-}
-function destructivelyRemoveElementFromBeginningOfArray (array, element) {
-  array.shift(element)
-  return []
-}
-function removeElementFromBeginningOfArray (array){
-  array.slice(1)
-  return []
-}
-function destructivelyRemoveElementFromEndOfArray (array) {
-  array.slice(-1)
-  return []
-}
-function destructivelyAddElementToEndOfArray (array) {
-   array.pop()
-   return[]
-}
-function removeElementFromEndOfArray (array) {
-  array.slice(0, array.length -1)
-  return []
+   array.unshift(element)
+   return [array]
 }
